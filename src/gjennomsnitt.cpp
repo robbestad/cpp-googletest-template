ec85286e9d1e71e371218795477494d522328033
Sven Anders Robbestad <svena@robbestad.com>
2019-11-27 13:40:23 +0100

    Update func to take in arguments

--- src/gjennomsnitt.cpp
+++ src/gjennomsnitt.cpp
@@ -5,8 +5,8 @@ using namespace std;
 double gjennomsnitt(int* tallrekke, int size){
 	int sum=0;
 	for(int i =0; i<size;i++){
-		i+1==size && std::cout << tallrekke[i];
-		i+1<size && std::cout << tallrekke[i] << ", ";
+		//i+1==size && std::cout << tallrekke[i];
+		//i+1<size && std::cout << tallrekke[i] << ", ";
 		sum+=tallrekke[i];
 	}
 	return (double) sum/size;
--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,20 @@
 
 
 int main(int argc, char **argv) {
-	int mylist [7] ={900, 12, 3, 17, 50, 282, -1};
-	int len = *(&mylist + 1) - mylist;
-	double snitt = gjennomsnitt(mylist, len);
-	std::cout << "\n\nGjennomsnitt: "<< snitt;
+	int size = argc - 1 ;
+	int* mylist  = (int*)calloc( size, sizeof(int) ) ;
+
+	if(size==0){
+		std::printf("Call with some numbers...\n");
+		return 0;
+	}
+    for( int i = 0; i < size; i++)
+    {
+        // convert to integer
+        std::sscanf( argv[i + 1], "%d", &mylist[i] ) ;
+    }
+	double snitt = gjennomsnitt(mylist, size);
+	std::printf("\nAverage: %d\n", snitt);
 	return 0;
 }
 
